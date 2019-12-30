f94e116f631f25d6ac855b5466538333254a851c
Alexander Greuter <32978721+alexandergreuter@users.noreply.github.com>
2019-12-30 19:40:46 +0100

    BROKEN

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -4,10 +4,12 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { AccountComponent } from "./account.component";
 import { TimetableComponent } from "~/app/account/timetable/timetable.component";
+import { TimefieldDetailComponent } from "~/app/account/timetable/timefield-detail/timefield-detail.component";
 
 const routes: Routes = [
     {path: "default", component: AccountComponent},
-    {path: "timetable", component: TimetableComponent}
+    {path: "timetable", component: TimetableComponent},
+    {path: "timetable/:day/:timefield", component: TimefieldDetailComponent}
 ];
 
 @NgModule({
--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,18 +1,24 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 import { AccountRoutingModule } from "./account-routing.module";
 import { AccountComponent } from "./account.component";
-import { TimetableComponent } from './timetable/timetable.component';
+import { TimetableComponent } from "./timetable/timetable.component";
+import { TimefieldDetailComponent } from "./timetable/timefield-detail/timefield-detail.component";
+import { TimeFormatPipe } from "~/app/shared/timetable.service";
 
 @NgModule({
     imports: [
         NativeScriptCommonModule,
-        AccountRoutingModule
+        AccountRoutingModule,
+        NativeScriptFormsModule
     ],
     declarations: [
         AccountComponent,
-        TimetableComponent
+        TimetableComponent,
+        TimefieldDetailComponent,
+        TimeFormatPipe
     ],
     schemas: [
         NO_ERRORS_SCHEMA
